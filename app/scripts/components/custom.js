d75fcc538254f2f0f456fca566ecc1248630da20
Vladimir Shevchun <vovashevchun@gmail.com>
2023-05-27 21:54:17 +0200

    end

--- app/scripts/components/custom.js
+++ app/scripts/components/custom.js
@@ -81,31 +81,6 @@ export default () => {
     $(this).siblings('.questions__item-body').slideToggle()
   })
 
-  // $('.header__mobile-menu').on('click', () => {
-  //   $('.header__mobile').addClass('active')
-  //   setTimeout(() => {
-  //     $('.header__mobile-box').addClass('active')
-  //   }, 300);
-  // })
-
-  // $('.header__mobile-overlay').on('click', () => {
-  //   $('.header__mobile-box').removeClass('active')
-  //   setTimeout(() => {
-  //     $('.header__mobile').removeClass('active')
-  //   }, 300);
-  // })
-
-  //   $('.lang__current').on('click', function lang(event) {
-  //     event.stopPropagation();
-  //     $(this).parents('.lang').toggleClass('active')
-  //   })
-
-  //   $(document).on("click", (event) => {
-  //     event.stopPropagation();
-  //     if ($(event.target).closest(".lang__list").length) return;
-  //     $('.lang').removeClass('active')
-  //   });
-
   function isFloat(value) {
     return typeof value === 'number' &&
       !Number.isNaN(value) &&
@@ -128,33 +103,4 @@ export default () => {
       }
     })
   })
-
-  //   $(window).on('scroll', function scrollPage() {
-  //     const heightWindow = 50
-  //     if ($(this).scrollTop() >= heightWindow) {
-  //       $('.header').addClass('header--scroll')
-  //     } else {
-  //       $('.header').removeClass('header--scroll')
-  //     }
-  //   })
-
-  //   $('.table-content__list a').on('click', function clickDownScroll() {
-  //     const marginTop = 110
-  //     const scrollEl = $(this).attr('href')
-  //     if ($(scrollEl).length !== 0) {
-  //       $('html, body').animate(
-  //         {
-  //           scrollTop: $(scrollEl).offset().top - marginTop,
-  //         },
-  //         500
-  //       )
-  //     }
-  //     return false
-  //   })
-
-  //   $('.js-scroll-top').on('click', () => {
-  //     $('html, body').animate({
-  //       scrollTop: 0
-  //     }, 500);
-  //   });
 }
